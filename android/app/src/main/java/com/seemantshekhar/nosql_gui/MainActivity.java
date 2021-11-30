b6442d821405c8764410a7a486374a6b41c301db
seemantshekhar43 <seemantshekhar43@gmail.com>
2021-12-01 00:20:51 +0530

    Project created

---
+++ android/app/src/main/java/com/seemantshekhar/nosql_gui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.seemantshekhar.nosql_gui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/seemantshekhar/nosql_gui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.seemantshekhar.nosql_gui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
